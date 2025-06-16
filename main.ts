a9ed32589403910a5f087a6f6bc07c1bdfda4630
tomas rada <Tomas.rada.024@pslib.cz>
2025-06-16 19:14:25 +0200

    Aktualizovat main.ts

--- main.ts
+++ main.ts
@@ -25,14 +25,14 @@ let vzdalenost: number = 0
 
 
 
-//// příjmání další odbočky
-//radio.onReceivedNumber(function (receivedNumber: number) {
-//    
-//    if (123380923 === radio.receivedPacket(RadioPacketProperty.SerialNumber)) {
-//        cesta = receivedNumber
-//        basic.showNumber(cesta)
-//    }
-//})
+// příjmání další odbočky
+radio.onReceivedNumber(function (receivedNumber: number) {
+    
+    if (123380923 === radio.receivedPacket(RadioPacketProperty.SerialNumber)) {
+        cesta = receivedNumber
+        basic.showNumber(cesta)
+    }
+})
 
 //otočka 90°
 function o90(strana: number) {
@@ -51,20 +51,7 @@ function o90(strana: number) {
 //j
 
 
-//odbočka 45°
-function o45(strana: number) {
-    if (strana === 1) {
-        jed(250, 0)
-        basic.pause(200)
 
-    }
-    else {
-        jed(0, 250)
-        basic.pause(200)
-
-    }
-
-}
 //jeď
 function jed(motor_p: number, motor_l: number) {
     basic.pause(20)
@@ -128,42 +115,16 @@ basic.forever(function () {
     //levá
     //else
     if (cesta === 1 && data_l === 1) {
-        music.play(music.tonePlayable(Note.C, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
-        basic.pause(500)
-        o45(1)
-        do {
-            jed(motor_rovne_p, motor_rovne_l)
-            data_c = pins.digitalReadPin(IR.c)
-            basic.pause(20)
-        } while (data_c === 0)
-        while (data_l === 0) {
-            jed(250, 0)
-            data_l = pins.digitalReadPin(IR.l)
-            basic.pause(20)
-        }
-        cesta = 0
+        o90(1)
     }
 
     //pravá
     else if (cesta === 2 && data_p === 1) {
-        o45(-1)
-        while (data_c === 0) {
-            jed(motor_rovne_p, motor_rovne_l)
-            data_c = pins.digitalReadPin(IR.c)
-            basic.pause(20)
-        }
-        while (data_p === 0) {
-            jed(0, 250)
-            data_p = pins.digitalReadPin(IR.p)
-            basic.pause(20)
-        }
-
-        cesta = 0
+        o90(2)
     }
     //rovně
     else if (data_c === 1 && cesta === 3) {
-        jed(motor_rovne_p, motor_rovne_l)
-        basic.pause(100)
+        jed
     }
 
 
@@ -171,29 +132,31 @@ basic.forever(function () {
 
 
 
-        //zabočení do prava
-        if (data_l === 1 && data_p === 0) {
-            jed(100, 0)
 
 
-        }
+    //zabočení do prava
+    if (data_l === 1 && data_p === 0) {
+        jed(100, 0)
 
-        //zabočení do leva
-        else if (data_l === 0 && data_p === 1) {
-            jed(0, 100)
 
-        }
+    }
 
-        else if (data_c === 1 && data_l === 0 && data_p === 0) {
-            // střed jede po čáře
-            jed(motor_rovne_p, motor_rovne_l)
-        }
-        else if (data_c === 1 && data_l === 1 && data_p === 0) {
-            // střed jede po čáře
-            jed(motor_rovne_p, motor_rovne_l)
-        }
-        else if (data_c === 1 && data_l === 0 && data_p === 0) {
-            jed(motor_rovne_p, motor_rovne_l)
+    //zabočení do leva
+    else if (data_l === 0 && data_p === 1) {
+        jed(0, 100)
+
+    }
+
+    else if (data_c === 1 && data_l === 0 && data_p === 0) {
+        // střed jede po čáře
+        jed(motor_rovne_p, motor_rovne_l)
+    }
+    else if (data_c === 1 && data_l === 1 && data_p === 0) {
+        // střed jede po čáře
+        jed(motor_rovne_p, motor_rovne_l)
+    }
+    else if (data_c === 1 && data_l === 0 && data_p === 0) {
+        jed(motor_rovne_p, motor_rovne_l)
         
 
         }
